2a3f1fab55d3b4b81f00406bb5e617710521449e
Aliaksei Liaukouski <Aliaksei.Liaukouski@instantconnectnow.com>
2025-12-01 10:27:44 +0100

    Added multiuser-chat module

---
+++ multiuser-chat/src/main/java/com/aliaukouski/multiuserchat/MultiuserChatApplication.java
@@ -0,0 +1,13 @@
+package com.aliaukouski.multiuserchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiuserChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiuserChatApplication.class, args);
+    }
+
+}
